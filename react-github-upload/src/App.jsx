9ab17129998306d9f8273caa42ecc572561bc12e
unknown <trahip5099@students.ecpi.edu>
2024-08-15 10:30:01 -0400

    Initial commit

---
+++ react-github-upload/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return(
+    <div>
+      <h1>Hello, React!</h1>
+      <p>This is a simple React applictation.</p>
+    </div>
+  );
+}
+
+export default App;
